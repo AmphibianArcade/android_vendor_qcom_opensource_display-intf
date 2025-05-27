fc69f025177e8aca6bb7c52a51d67c5096454d25
Chintankumar Savaliya <csavaliy@qti.qualcomm.com>
2025-05-27 14:36:11 +0530

    display-intf: Sub Pixel Sampling Shader
    Change-Id: Idf9458139ae89b98a97bc1b068e8ef0fbb1bf37e

--- sdmclient/sdm_compositor_sideband_cb_intf_v2.h
+++ sdmclient/sdm_compositor_sideband_cb_intf_v2.h
@@ -16,7 +16,8 @@ public:
 
   // gpu display correction
   virtual int DisplayCorrectionBlit(uint64_t display, const void *dst,
-                                    const void *src, int32_t src_fence_fd) = 0;
+                                    const void *src, int32_t src_fence_fd,
+                                    int32_t flip_X, int32_t flip_Y) = 0;
   virtual int InitDisplayCorrection(uint64_t display,
                                     uint64_t display_correction_config_type,
                                     bool is_secure) = 0;
